fe2f6c3c58aeffee96848a5fe1a66b69788f9c70
Adam Yafout <adam.yafout@gmail.com>
2021-10-11 19:04:00 -0400

    STUD-65: Creating tables related to the When2meet module

---
+++ when2meet.sql
@@ -0,0 +1,52 @@
+CREATE TABLE when2meet(
+W2W_ID VARCHAR(10) primary key not null,
+W2W_name VARCHAR(10)
+);
+
+create table student(
+sid VARCHAR(10) primary key not null,
+name VARCHAR(10),
+W2W_ID VARCHAR(10),
+foreign key (W2W_ID) references when2meet(W2W_ID)
+);
+
+
+create table possibilities( 
+PID VARCHAR(10) primary	key not null,
+PDay VARCHAR(10),
+start_Time integer,
+end_Time integer
+);
+
+-- DROP TABLE possibilities;
+
+
+
+create table Availibilities(
+AID VARCHAR(10) primary	key not null,
+PDay VARCHAR(10),
+start_Time integer,
+end_Time integer
+);
+
+-- DROP TABLE Availibilities;
+
+create table sets_possibilities( 
+W2W_ID VARCHAR(10),
+PID VARCHAR(10),
+FOREIGN key (W2W_ID) references when2meet(W2W_ID),
+FOREIGN key (PID) references Possibilities(PID),
+primary key (W2W_ID, PID)
+);
+
+-- DROP TABLE sets_possibilities;
+
+create table choose_availibilities(
+SID VARCHAR(10),
+AID VARCHAR(10),
+FOREIGN key (SID) references Student(SID),
+FOREIGN key (AID) references Availibilities(AID),
+primary key (SID, AID)
+);
+
+-- DROP TABLE choose_availibilities;
---
+++ when2meet_inserts.sql
@@ -0,0 +1,32 @@
+
+insert into when2meet values('1x2', '490');
+insert into when2meet values('2Y3', '321');
+insert into when2meet values('3Z4', '479');
+insert into when2meet values('4A5', '363');
+
+insert into student values('123456','Adam', '2Y3');
+insert into student values('678901','Zubair', '2Y3');
+insert into student values('789012','Amine', '3Z4');
+insert into student values('684920','Mark', '3Z4');
+
+insert into Availibilities values('1234', 'Monday','10','12');
+insert into Availibilities values('2345', 'Monday','12','14');
+insert into Availibilities values('3456', 'Friday','11','18');
+insert into Availibilities values('4567',  'Wednesday','18','22');
+
+insert into Possibilities values('qwer', 'Monday','12','14');
+insert into Possibilities values('asdf', 'Wednesday','10','16');
+insert into Possibilities values('zxcv', 'Wednesday','8','23');
+insert into Possibilities values('qwas', 'Friday','18','20');
+
+insert into sets_possibilities values('1x2','qwer');
+insert into sets_possibilities values('2Y3','qwer');
+insert into sets_possibilities values('4A5','asdf');
+insert into sets_possibilities values('4A5','zxcv');
+
+insert into choose_availibilities values('678901','1234');
+insert into choose_availibilities values('789012','1234');
+insert into choose_availibilities values('789012','4567');
+insert into choose_availibilities values('123456','3456');
+
+
