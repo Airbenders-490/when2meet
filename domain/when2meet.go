9c8dec8534deebbf415e968395c4d9c9a0cb7235
Adam Yafout <adam.yafout@gmail.com>
2021-10-12 16:20:43 -0400

    STUD-83: modifying when2meet.go and main.go

--- domain/when2meet.go
+++ domain/when2meet.go
@@ -2,13 +2,13 @@ package domain
 
 import "context"
 
-// When2meet struct
+// when2meet struct
 type When2meet struct {
 	W2M_ID   string `json:"id"` //uuid string
 	W2M_name string `json:"name"`
 }
 
-// When2meetUseCase interface defines the functions all When2meetUseCases should have
+// When2meetUseCase interface defines the functions all when2meetUseCases should have
 type When2meetUseCase interface {
 	Create(ctx context.Context, st *When2meet) error
 	GetByID(ctx context.Context, id string) (*When2meet, error)
@@ -16,7 +16,7 @@ type When2meetUseCase interface {
 	Delete(ctx context.Context, id string) error
 }
 
-// When2meetRepository interface defines the functions all When2meetRepositories should have
+// When2MeetRepository interface defines the functions all when2meetRepositories should have
 type When2meetRepository interface {
 	Create(ctx context.Context, id string, st *When2meet) error
 	GetByID(ctx context.Context, id string) (*When2meet, error)
--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"context"
+	"log"
+
+	"github.com/airbenders/when2meet/domain"
+	"github.com/airbenders/when2meet/when2meet/repository"
+	"github.com/jackc/pgx/v4/pgxpool"
+)
+
 func main() {
+
+	connectionstr := "user=postgres dbname=when2meet password=Team7SOEN363 host=local sslmode=disable"
+	pool, err := pgxpool.Connect(context.Background(), connectionstr)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	repository := repository.NewWhen2meetRepository(pool)
+	when2meet := &domain.When2meet{
+		W2M_ID:   "1994",
+		W2M_name: "meetings",
+	}
+	repository.Create(context.Background(), when2meet.W2M_ID, when2meet)
 
 }
